c3f86a3b3e55d2ecc0762d74309dc29c5b798e58
Dan McCarthy <wererabbbit@yahoo.com>
2017-10-07 23:25:37 -0500

    JS: Added swapImageData()
    This function swaps the pixel objects at two indices in the image array.
    It swaps the data both in that array and swaps their corresponding
    pixels on the canvas.

--- sort.js
+++ sort.js
@@ -26,5 +26,29 @@ function initializeImageArray()
 	}
 }
 
+function swapImageData(index1, index2)
+{
+	var canvas = document.getElementById("imageCanvas");
+	var context = canvas.getContext("2d");
+	
+	//Swap in canvas data
+	var colorA = image[index1].color;
+	var colorB = image[index2].color;
+	
+	var x1 = get2DXValue(index1, canvas.width);
+	var y1 = get2DYValue(index1, canvas.width);
+	
+	var x2 = get2DXValue(index2, canvas.width);
+	var y2 = get2DYValue(index2, canvas.width);
+	
+	context.putImageData(colorB, x1, y1);
+	context.putImageData(colorA, x2, y2);
+	
+	//Swap in image array
+	var c = image[index1];
+	image[index1] = image[index2];
+	image[index2] = c;
+}
+
 	
 }
