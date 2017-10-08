30bf06bea4c36ac25bcf7e9f2a9bedff8fa5c09f
Dan McCarthy <wererabbbit@yahoo.com>
2017-10-07 23:23:28 -0500

    JS: Added initializeImageArray()
    This function is designed to keep an array that keeps track of the image
    data. The data on the canvas is not enough information to be able to
    sort the pixels back into the original image. Therefore this function is
    required to attribute an index to each pixel and store the object
    containing the color and index in an array.

--- sort.js
+++ sort.js
@@ -11,5 +11,20 @@ function loadImage()
 	}
 	
 	imageData.src = "chicken.png";
+
+function initializeImageArray()
+{
+	var canvas = document.getElementById("imageCanvas");
+	var context = canvas.getContext("2d");
+	
+	var pixelCount = canvas.width * canvas.height;
+	
+	for(var i = 0; i < pixelCount; i++)
+	{
+		var pixel = context.getImageData(get2DXValue(i, canvas.width), get2DYValue(i, canvas.width), 1, 1);
+		image.push({index:i, color:pixel});
+	}
+}
+
 	
 }
