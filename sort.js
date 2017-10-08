ce28ff3923b88eb9d748a5e4db2f6535b389e742
Dan McCarthy <wererabbbit@yahoo.com>
2017-10-07 23:40:50 -0500

    JS: Massive Improvement to shuffleCanvas() Function
    Previously the shuffleCanvas function created an array of indices,
    selected two at random, swapped the data at those indices, then removed
    the indices from the array so that they would not get selected again,
    then repeated until the array of indices was empty.
    
    This was needlessly resource demanding and overly complex.
    
    The algorithm has been replaced to iterate through the array and select
    a random index between the current index and the end of the array and
    swapped the two, then repeated for the next index, until it iterated
    through the full data array.

--- sort.js
+++ sort.js
@@ -66,44 +66,10 @@ function shuffleCanvas()
 	var context = canvas.getContext("2d");
 	
 	var pixelCount = canvas.width * canvas.height;
-	
-	var elementIndices = [];
-	
+
 	for(var i = 0; i < pixelCount; i++)
-	{
-		elementIndices.push(i);
-	}
-	
-	for(var i = 0; i < elementIndices.length; i++)
-	{
-		var randomIndex1 =  Math.floor(Math.random() * (elementIndices.length-1));
-		
-		var randomIndex2 =  Math.floor(Math.random() * (elementIndices.length-1));
-		
-		if(randomIndex1 == randomIndex2)
-		{
-			if(randomIndex2 < 0)
-			{
-				randomIndex2--;
-			}
-			else if(randomIndex1 < 0)
-			{
-				randomIndex1--;
-			}
-			else if(randomIndex1 < (elementIndices.length - 1))
-			{
-				randomIndex1++;
-			}
-			else if(randomIndex2 < (elementIndices.length - 1))
-			{
-				randomIndex2++;
-			}
-		}
-		
-		swapImageData(randomIndex1, randomIndex2);
-		
-		elementIndices.splice(randomIndex1, 1);
-		elementIndices.splice(randomIndex2, 1);
+		var randomIndex =  Math.floor(Math.random() * ((pixelCount-1) - i) + i);
+		swapImageData(i, randomIndex);
 	}
 }
 
