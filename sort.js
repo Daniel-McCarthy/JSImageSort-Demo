15b6b43fa96024b0607ef38129b4831a0965f86a
Dan McCarthy <wererabbbit@yahoo.com>
2017-10-07 23:21:41 -0500

    JS: Renamed loadImage()'s image variable to imageData to Remove Ambiguity

--- sort.js
+++ sort.js
@@ -4,13 +4,12 @@ function loadImage()
 	var imageCanvas = document.getElementById("imageCanvas");
 	var imageCanvasContext = imageCanvas.getContext("2d");
 	
-	var image = new Image();
+	var imageData = new Image();
 	
-	image.onload = function () {
-		imageCanvasContext.drawImage(image, 0, 0);
+	imageData.onload = function () {
+		imageCanvasContext.drawImage(imageData, 0, 0);
 	}
 	
-	image.src = "chicken.png";
+	imageData.src = "chicken.png";
 	
 }
-
