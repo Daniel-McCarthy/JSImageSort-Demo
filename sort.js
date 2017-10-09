8b0262a96d0b2062a993f31ba467adfa1703a4f2
Dan McCarthy <wererabbbit@yahoo.com>
2017-10-08 21:12:42 -0500

    JS: StartSort() Now Calls loadImage()
    This is done to make sure that the correct photo is loaded based on
    selection when sorting begins

--- sort.js
+++ sort.js
@@ -39,6 +39,8 @@ function startSort()
 	clearInterval(sortInterval);
 	sorted = false;
 	
+	loadImage();
+	
 	shuffleCanvas();
 	
 	var algorithm = document.getElementById("algorithmDropDownMenu").value;
